bc56cda1edc5e063173d189c3351f22e69140fde
Kaveh Saleminejad <lcaveh@gmail.com>
2018-07-27 23:02:33 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array) {
+ for (var i = 0; i < 25; i++) {
+  if (i===0) {
+    array.push("I am 1 strange loop.")
+  }
+  else {
+    array.push("I am ${i+1} strange loops.")
+  }
+ }
+ return array;
+}
+function whileLoop(n) {
+ while (n>0) {
+  if (n===0) {
+    console.log('done');
+  }
+  else {
+    console.log(n);
+  }
+  n--;
+ }
+}
